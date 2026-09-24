54e6c5d53b635f509745ffe45619d4cba8bcaea5
Kenneth <kenneththai100@gmail.com>
2026-09-24 15:59:53 -0500

    bug fix

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,10 @@
-from api_activity import app
+from api_activity import create_app
 import pytest
 
 
 @pytest.fixture
 def client():
+    app = create_app()
     with app.test_client() as client:
         yield client
         
